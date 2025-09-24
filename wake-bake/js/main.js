8c2b722fe82e62431f63af272fa880a363e5b755
tkatisha <t.katishaaa@mail.ru>
2025-09-25 00:14:15 +0300

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -108,6 +108,36 @@
         })
     })
 
+    // Слайдер-галерея
+
+    const swiper = new Swiper('.gallery__slider', {
+
+        spaceBetween: 15,
+        slidesPerView: 1.5,
+
+        pagination: {
+            el: '.gallery__pagination',
+            type: 'fraction',
+        },
+
+        navigation: {
+            nextEl: '.gallery__next',
+            prevEl: '.gallery__prev',
+        },
+
+        breakpoints: {
+            601: {
+                slidesPerView: 3
+            },
+            801: {
+                spaceBetween: 32,
+            },
+            1101: {
+                slidesPerView: 4
+            },
+        },
+    });
+
 
 
 })()
