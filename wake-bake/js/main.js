e6f44340a34a34242515d8b8f3be21f640f53063
tkatisha <t.katishaaa@mail.ru>
2025-09-27 13:01:47 +0300

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -166,6 +166,11 @@
         },
     });
 
+    // Маска для телефона
+    
+    const telInputs = document.querySelectorAll('input[type="tel"]')
+    const im = new Inputmask('+7 (999) 999-99-99')
+    im.mask(telInputs)
 
 })()
 
